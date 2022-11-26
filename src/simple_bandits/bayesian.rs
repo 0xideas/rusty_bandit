3eb6346c5933b5d2a9475cff5fa411a3956733d3
Leon Luithlen <leontimnaluithlen@gmail.com>
2022-11-26 15:05:59 +0100

    Fix bayesian act method

--- src/simple_bandits/bayesian.rs
+++ src/simple_bandits/bayesian.rs
@@ -2,7 +2,7 @@ use statrs::distribution::Beta;
 use rand::{Rng, SeedableRng};
 use rand::rngs::StdRng;
 use rand::RngCore;
-
+use rand::distributions::Distribution;
 
 pub struct BayesianBandit{
     n_arms: i8,
@@ -15,12 +15,12 @@ impl BayesianBandit{
     fn new(n_arms: i8) -> BayesianBandit {
         let n_arms_size: usize = n_arms.try_into().unwrap();
         let beta = Beta::new(1.0, 1.0).unwrap();
-        let mut arms: Vec<Beta> =  vec![beta; n_arms_size];
-        let mut rng = StdRng::seed_from_u64(101);
+        let arms: Vec<Beta> =  vec![beta; n_arms_size];
+        let rng = StdRng::seed_from_u64(101);
         BayesianBandit{ n_arms, arms, rng}
     }
 
-    fn update(&self, arm: i8, success: bool) -> () {
+    fn update(mut self, arm: i8, success: bool) -> () {
         let arm_size: usize = arm.try_into().unwrap();
         let beta = self.arms[arm_size];
         if success {
@@ -33,9 +33,9 @@ impl BayesianBandit{
     }
 
     fn act<R: Rng + ?Sized>(&self, rng: &mut R) -> i8 {
-        let samples = self.arms.iter().map(|beta| -> f64 {beta.sample(rng)});
-        let max_value = samples.fold(0.0f64, |max, val| if val > max{ val } else{ max });
-        let largest = samples.position(|r| r == max_value).unwrap();
+        let mut samples: Vec<u64> = self.arms.iter().map(|beta| -> u64 {beta.sample(rng).to_bits()}).collect();
+        let max_value: u64 = samples.iter().fold(0, |max: u64, val: &u64| if val > &max{ *val } else{ max });
+        let largest = samples.iter().position(|r: &u64| r == &max_value).unwrap();
         let largest_int: i8 = largest.try_into().unwrap();
         largest_int
     }
